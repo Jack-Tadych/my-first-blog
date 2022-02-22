2a791e8f7eabf249fcc76f017c300c57c89d9f8f
Jack-Tadych <tadyc1ja@cmich.edu>
2022-02-22 16:30:59 -0500

    another test

--- mysite/settings.py
+++ mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 #source myvenv/bin/activate
 import os
-from django.urls import path
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -120,7 +120,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'static'
+#STATIC_ROOT = BASE_DIR / 'static'
 
 #howdy
 #STATIC_ROOT = BASE_DIR / 'static'
