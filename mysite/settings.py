a51aa7370a031d3888789d9bd4cbe48d5a391faf
Jack-Tadych <tadyc1ja@cmich.edu>
2022-02-22 16:44:26 -0500

    changed static root

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 #source myvenv/bin/activate
 import os
 from django.urls import path
-
+from django.conf.urls import include
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
