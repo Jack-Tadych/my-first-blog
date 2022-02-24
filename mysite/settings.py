527b2e49636851b6b6ddb2716fee4ed84ed1fc7b
Jack-Tadych <tadyc1ja@cmich.edu>
2022-02-24 15:21:21 -0500

    added / in the static_root

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #STATIC_ROOT = BASE_DIR / 'static'
 #STATIC_ROOT = os.path.join(BASE_DIR, "/static")
 #howdy
