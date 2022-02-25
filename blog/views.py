2f6a026ed45e6c1537f2e0b3b061691f40e73f91
Jack-Tadych <tadyc1ja@cmich.edu>
2022-02-25 15:06:35 -0500

    displaying python info

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,8 @@
-from django.http import HttpResponse
 from django.shortcuts import render
-from .models import Author, Tag, Category, Post
+from django.utils import timezone
+from .models import Post
 
-
-def index(request):
-    return HttpResponse("Hello Django")
-
-
-# view function to display a list of posts
 def post_list(request):
-    posts = Post.objects.all()
+    #posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts =Post.objects.all()
     return render(request, 'blog/post_list.html', {'posts': posts})
